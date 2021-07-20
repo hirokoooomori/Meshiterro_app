e1e22731aa1056eb4cf02647dc8ed6a8a26c61f3
hirokoooomori <hirouta0408@gmail.com>
2021-07-20 07:25:14 +0000

    [Update]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5cd19d8b64a88bfa14a42f13cf100f43fe0f484a3297aeb1951b5958a207ef89b3d117616e4d29d0355cdc583a12e558b262dad885450b0558b049f33ad8e907'
